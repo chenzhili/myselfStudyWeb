6de93a0a66e7bb47d90dcd7dc847f292fc6b3e8e
wozaizhelo <1845882620@qq.com>
2017-05-11 23:00:57 +0800

    美观的input做法，还没做

--- ionic_test/www/js/controllers.js
+++ ionic_test/www/js/controllers.js
@@ -366,13 +366,13 @@ angular.module('starter.controllers', [])
 
   /*promise方法*/
   /*const preloadImage = function (path) {
-return new Promise(function (resolve, reject) {
-var image = new Image();
-image.onload = resolve;
-image.onerror = reject;
-image.src = path;
-});
-};*/
+  return new Promise(function (resolve, reject) {
+  var image = new Image();
+  image.onload = resolve;
+  image.onerror = reject;
+  image.src = path;
+  });
+  };*/
   
   $rootScope.disState = 1;
 })
