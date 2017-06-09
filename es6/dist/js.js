575782cb694d92e8db7b73bd03d286292b91fc84
curry <17828455424@163.com>
2017-06-09 12:00:04 +0800

    工作中jquery的用途

--- es6/dist/js.js
+++ es6/dist/js.js
@@ -1,96 +1,70 @@
 "use strict";
 
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
-function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
-
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-var dir = document.querySelector(".div");
-var btn = document.querySelector("button");
-var a = 1;
+var _obj1$obj = { obj1: "tom", obj2: "23" }; /*let dir = document.querySelector(".div");
+                                             let btn  = document.querySelector("button");
+                                             const a = 1;*/
 
 /*构造函数*/
-var f = function f() {
-    console.log(1 + 1);
+/*let f = ()=>{
+    console.log(1+1);
 };
-btn.onclick = function () {
-    dir.innerHTML = "<p>" + a + "</p>";
-    carry.says(60);
-    guruda.call("sdfd");
+btn.onclick =  () => {
+  dir.innerHTML = `<p>${a}</p>`;
+  carry.says(60);
+  guruda.call("sdfd");
     console.log(Boolean(d == 1));
     console.log(Boolean(b == 2));
     console.log(Boolean(c == 3));
     console.log(obj);
-    fun("", [true, 12]);
-    f.apply(undefined, [0].concat([1, 2]));
+    fun( "",[true,12]);
+    f(0,...[1,2]);
 };
-
-var Name = function () {
-    function Name() {
-        _classCallCheck(this, Name);
-
+class Name {
+    constructor() {
         this.name = "tom";
     }
-
-    _createClass(Name, [{
-        key: "says",
-        value: function says(age) {
-            console.log(this.name + "\u5E74\u9F84\u4E3A" + age + "\u5C81\u5417");
-        }
-    }]);
-
-    return Name;
-}();
-
-var carry = new Name();
-
-var NameAo = function (_Name) {
-    _inherits(NameAo, _Name);
-
-    function NameAo() {
-        _classCallCheck(this, NameAo);
-
-        var _this = _possibleConstructorReturn(this, (NameAo.__proto__ || Object.getPrototypeOf(NameAo)).call(this));
-
-        _this.name = "curry";
-        return _this;
+    says(age){
+        console.log(`${this.name}年龄为${age}岁吗`);
     }
-
-    _createClass(NameAo, [{
-        key: "call",
-        value: function call(_call) {
-            this.says(_call);
-        }
-    }]);
-
-    return NameAo;
-}(Name);
-
-var guruda = new NameAo();
+}
+let carry = new Name();
+class NameAo extends Name{
+    constructor(){
+      super();
+      this.name = "curry";
+    }
+    call(call){
+      this.says(call);
+    }
+}
+let guruda = new NameAo();*/
 
 /*解构*/
-var d = 1,
-    b = 2,
-    c = 3;
-
-var cat = "tom";
-var dog = "lili";
-var obj = { cat: cat, dog: dog };
-function fun(x) {
-    for (var _len = arguments.length, y = Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
-        y[_key - 1] = arguments[_key];
-    }
-
-    console.log(y.length);
-    console.log(y);
+/*let [d,b,c]=[1,2,3];
+let cat = "tom";
+let dog = "lili";
+let obj ={cat,dog};
+function fun (x,...y){
+  console.log(y.length);
+  console.log(y);
 }
-function f(x, y) {
-    console.log(x);
-    console.log(y);
-    console.log(x + y);
-    console.log(String(y));
-}
+function f(x,y){
+  console.log(x);
+  console.log(y);
+  console.log(x+y);
+  console.log(String(y));
+}*/
+/*对于变量赋值时的let和const*/
+//1.let下的块级作用域中，不存在申明提前的说法了，并且不能进行重复申明
+//2.const和let相似，但是对于他只能赋值一次
+
+/*对于解构，数组解构和对象解构*/ //对于写法上不知道的 ([a,b]=[1,3]);  ({a,b} = {1,2})
+//对于解构我不知道的
+//对于对象解构，可以给他换变量名 
+
+name = _obj1$obj.obj1;
+age = _obj1$obj.obj2;
+
+console.log(name);
+console.log(age);
 //# sourceMappingURL=js.js.map
