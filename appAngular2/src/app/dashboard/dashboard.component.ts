538c35a0810fbe58a4f0db21554c389f34c5fa2e
curry <17828455424@163.com>
2017-06-14 10:03:52 +0800

    修改提交

--- appAngular2/src/app/dashboard/dashboard.component.ts
+++ appAngular2/src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, OnChanges,Input} from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent implements OnInit {
-
+export class DashboardComponent implements OnInit,OnChanges {
+  hero = 1;
   constructor() { }
-
+  addDom(){
+    document.getElementsByTagName("p")[0].innerHTML = `<a>增加a标签${this.hero}</a>`;
+  }
   ngOnInit() {
+    console.log("init"+"是否先出现");
+  }
+  ngOnChanges(){
+    console.log("change"+"是否先出现");
   }
 
 }
