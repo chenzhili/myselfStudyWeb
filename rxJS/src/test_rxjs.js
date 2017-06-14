88be40fdff5408fefb11afa88b32ccd6fe726e0c
curry <17828455424@163.com>
2017-06-14 20:22:30 +0800

    rxjs提交

--- rxJS/src/test_rxjs.js
+++ rxJS/src/test_rxjs.js
@@ -2,7 +2,13 @@ const obser = Rx.Observable.create(function(observer){
     console.dir(observer);
     observer.next(1);
     observer.next(2);
-
+    observer.complete();
+    setTimeout( () =>{
+        console.log("是否运行:"+4);
+        observer.next(4);
+        observer.complete();
+    },1000);
 });
-obser.subscribe();
-console.dir(Rx.Observable);
+obser.subscribe(x =>{
+    console.log(x);
+});
