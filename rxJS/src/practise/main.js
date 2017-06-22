86df2d5fdea1b0795b8e11030562d586036538ed
curry <17828455424@163.com>
2017-06-22 18:41:53 +0800

    rxjs提交

--- rxJS/src/practise/main.js
+++ rxJS/src/practise/main.js
@@ -1,5 +1,13 @@
 var input = document.getElementById("input");
-var obserable = Rx.Observable.fromEvent(input,"keyup");
-obserable.subscribe(x=>{
-	console.log(x);
+var source = Rx.Observable.fromEvent(input,"keyup");
+/*var subject = new Rx.Subject();
+var refCounted = source.multicast(subject).refCount();*/
+source
+	.debounceTime(500)
+	.subscribe(x=>{
+	if(x.keyCode != 67 && x.keyCode != 17){
+        if(x.target.value){
+            console.log(x.target.value);
+		}
+	}
 });
