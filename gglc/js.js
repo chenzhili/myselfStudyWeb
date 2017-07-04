feb5ddb905e5a52264ead0b2257a29262ec15846
curry <17828455424@163.com>
2017-07-04 18:15:17 +0800

    gg乐彩提交

---
+++ gglc/js.js
@@ -0,0 +1,19 @@
+/**
+ * Created by YK on 2017/7/4.
+ */
+$(document).ready(function(){
+    makeQr("code","http://www.baidu.com");
+    $(".close_modal").click(function(){
+        $(".modal").css("display","none");
+    });
+    $(".help").click(function(){
+        $(".modal").css("display","block");
+    });
+});
+function makeQr(ele,http){
+    $("#"+ele)[0].innerHTML = "";
+    new QRCode($("#"+ele)[0],{
+        width:"140",
+        height:"140"
+    }).makeCode(http);
+}
