528d3ebeb96e37de6672c37dd1037a7c10365769
curry <17828455424@163.com>
2017-07-12 18:55:26 +0800

    webpack的修改

--- exercise-webpack/webpack.config.js
+++ exercise-webpack/webpack.config.js
@@ -1,26 +1,27 @@
 module.exports = {
 	devtool:"eval-source-map",
 	entry:{
-		"./css/":__dirname+"/app/main1.css", //这种写法的好处是 对应的key能够当做output输出口的文件的路径存在，这样就上输出的文件可以灵活的在不同的位置
-		"./public/":__dirname+"/app/main.js"
+		"./css/style":__dirname+"/app/main1.css", //这种写法的好处是 对应的key能够当做output输出口的文件的路径存在，这样就上输出的文件可以灵活的在不同的位置
+		"./public/bundle":__dirname+"/app/main.js"
 	},
 	output:{
 		path:__dirname,
-		filename:"[name]bundle.js"
+		filename:"[name].js"
 	},
 
 	/*本地服务器*/
-	 devServer: {
+	 devServer: { 
 	    contentBase: "./public",//本地服务器所加载的页面所在的目录
 	    historyApiFallback: true,//不跳转
 	    inline: true//实时刷新
   	},
 
   	module:{
-  		rules:[
+  		rules:[ //rules和loaders效果一样，但是现在3点几的版本用的是rules
   			{
   				test:/\.css$/,
-  				loader:'style-loader!css-loader'
+  				loader:'style-loader!css-loader',
+  				exclude:"/node_modules/"
   			}
   		]
   	}
