6fdbc73777fc62f25ae7808179dc22798e8c45a5
curry <17828455424@163.com>
2017-11-21 14:43:36 +0800

    barrage change

--- barrage_danmu/js/index.js
+++ barrage_danmu/js/index.js
@@ -1,77 +1,104 @@
 window.onload = function(){
-  /*css3实现弹幕，当然完整版还需要 对数据的随机处理*/
-  let container = document.getElementsByClassName("floor_container")[0];
-  container.className = "floor_container barrage";
-  let width = parseFloat(getComputedStyle(container).width);
-  container.style.right = -width+"px";
-  container.style.animationDuration = width*0.025+"s";
-  /*用js实现弹幕*/
-  /*let obj = {
-    distance:"", //移动总长度
-    duration:0, //每一步的时间
-    step:0, //总步数
-    stepDis:10, //一步的距离
-  };
-  let container2 = document.getElementsByClassName("floor_container2")[0];
-  obj.distance = parseFloat(getComputedStyle(container2).width);
-  obj.step = obj.distance/obj.stepDis;*/
-  class Barrage{
-    constructor(container2){
-      this.distance = Math.abs(parseFloat(getComputedStyle(container2).width))+parseFloat(window.innerWidth);
-      this.stepDis = 2;
-      this.step = Math.ceil(this.distance/this.stepDis);
-      this.n = 0;
+    /*css3实现弹幕，当然完整版还需要 对数据的随机处理*/
+    function animationC3(containerFloor,key){
+        let container_floor = document.getElementsByClassName(containerFloor)[0];
+        container_floor.className = `floor_container${key} barrage`;
+        let width = parseFloat(getComputedStyle(container_floor).width);
+        container_floor.style.right = -width+"px";
+        container_floor.style.animationDuration = width*0.025+"s";
     }
-    init(){
-      this.begin(this);
+    /*js实现弹幕*/
+    class Barrage{
+        constructor(container,speed){
+            this.container = container;
+            this.distance = Math.abs(parseFloat(getComputedStyle(container).width))+parseFloat(window.innerWidth);
+            this.stepDis = speed;
+            this.step = Math.ceil(this.distance/this.stepDis);
+            this.n = 0;
+        }
+        init(){
+            this.begin(this);
+        }
+        begin(me){
+            me = this;
+            if(me.n == me.step)return;
+            /*主要逻辑*/
+            me.fun();
+            me.n++;
+            me.time = setTimeout(()=>{
+                me.begin(me);
+            },me.stepDis*25)
+        }
+        fun(){
+            this.container.style.right = parseFloat(this.container.style.right) + this.stepDis + "px";
+        }
     }
-    begin(me){
-        me = this;
-        if(me.n == me.step)return;
-      /*主要逻辑*/
-        me.fun();
-        me.n++;
-        me.time = setTimeout(()=>{
-            me.begin(me);
-        },me.stepDis*25)
-    }
-    fun(){
-      container2.style.right = parseFloat(container2.style.right) + this.stepDis + "px";
-    }
-  }
-  let container2 = document.getElementsByClassName("floor_container2")[0];
-  container2.style.right = -parseFloat(getComputedStyle(container2).width)+"px";
-  let barrage = new Barrage(container2);
-  barrage.init();
-  getData().then(function(data){
-    console.log(data);
-  }).catch(function(err){
-    console.log(err);
-  });
+
+    /*模拟获取数据，进行完成测试*/
+    let obj = {};
+    getData().then(function(data){
+        if(data.result){
+            obj = data.data;
+            // console.log(Object.keys(obj));
+            let container = document.getElementsByClassName("container")[0];
+            let fregment = document.createDocumentFragment();
+            for(let key in obj){
+                let div = document.createElement("div");
+                div.className = "floor";
+                let spanArray = "";
+                if(obj[key].length){
+                    for(let i=0;i<obj[key].length;i++){
+                        spanArray += `<span>${obj[key][i]}</span>`
+                    }
+                }
+                div.innerHTML = `<div class="floor_container${key}">${spanArray}</div>`;
+                fregment.appendChild(div);
+            }
+            container.appendChild(fregment);
+            /*进行弹幕播放*/
+            for(let key in obj){
+                /*css3实现弹幕，当然完整版还需要 对数据的随机处理*/
+                // animationC3(`floor_container${key}`,key);
+                /*js实现弹幕*/
+                let speed = 0;
+                if(key%2 == 0){
+                    speed = 1;
+                }else{
+                    speed = 2;
+                }
+                let containerFloor = document.getElementsByClassName(`floor_container${key}`)[0];
+                containerFloor.style.right = -parseFloat(getComputedStyle(containerFloor).width)+"px";
+                let barrage = new Barrage(containerFloor,speed);
+                barrage.init();
+            }
+        }
+    }).catch(function(err){
+        console.log(err);
+    });
 
 };
 
 /*请求接口*/
 function getData(){
-  return new Promise(function(resolve,reject){
-      let xml = new XMLHttpRequest();
-      xml.open("GET","json/adv.json",true);
-      xml.send();
-      xml.onreadystatechange = function(){
-          if(xml.readyState == 4 && xml.status == 200){
-              if(xml.responseText){
-                  resolve({
-                    result:1,
-                    data:JSON.parse(xml.responseText),
-                    mess:"获取数据成功"
-                  });
-              }else{
-                reject({
-                    result:0,
-                    message:"获取不到数据"
-                });
-              }
-          }
-      };
-  });
+    return new Promise(function(resolve,reject){
+        let xml = new XMLHttpRequest();
+        xml.open("GET","json/adv.json",true);
+        xml.send();
+        xml.onreadystatechange = function(){
+            if(xml.readyState == 4 && xml.status == 200){
+                if(xml.responseText){
+                    resolve({
+                        result:1,
+                        data:JSON.parse(xml.responseText),
+                        mess:"获取数据成功"
+                    });
+                }else{
+                    reject({
+                        result:0,
+                        message:"获取不到数据"
+                    });
+                }
+            }
+        };
+    });
 }
