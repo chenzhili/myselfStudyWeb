8b7512ee32226afa3ba939563ce5e960da3226f6
curry <17828455424@163.com>
2018-01-08 15:35:46 +0800

    react change

--- first_react_project/README.md
+++ first_react_project/README.md
@@ -68,6 +68,13 @@
             //而在以前，如果需要传递数组当参数，我们需要使用函数的apply方法
             sayHello.apply(null,people);//输出：Hello Wayou,John,Sherlock
     */
+2018/1/8
+    /*
+        1、这不是 es6，但是比较中要 对于 call,apply,bind的区别：
+            都是 为了 改变 调用 函数 的 作用环境（作用是改变函数中的this指向）（就是 可以 强行 让 某一个 来调用这个方法）
+            注意：bind 是返回对应函数，便于稍后调用；apply 、call 则是立即调用 。
+    */
+
 */
 
 /*
@@ -80,6 +87,74 @@
         印象：react 就是一个 js 库，减少 html 的 Dom 操作，加快 渲染 速度；
             引出了两种 扩展语法：
                 jsx（js的扩展），虚拟dom （就是 react自己的 dom，最后渲染成 html的 dom 只是 跟新对应 的修改部分）
+    */
+2018/1/8
+    /*
+        1、创建组件 可以通过 函数式 和 es6的类；
+            es6 可以 用 状态(state) 来让组件 重新渲染，并且这个是 组件 的私有属性
+
+            对于 状态的 要点：正确的使用状态
+            1、不要直接更新状态
+                不能直接用 : this.state.key = value（这种更新不了）构造函数是唯一能够初始化 this.state 的地方。
+                api:是用 this.setState({
+                                                key : value
+                                            });
+            2、状态更新可能是异步的 （setState中可以 用 函数来进行更新）
+                // Wrong
+                this.setState({
+                  counter: this.state.counter + this.props.increment,
+                });
+                // Correct
+                this.setState((prevState, props) => ({ //上一个 状态值
+                  counter: prevState.counter + props.increment
+                }));
+                // Correct
+                this.setState(function(prevState, props) {
+                  return {
+                    counter: prevState.counter + props.increment
+                  };
+                });
+            3、状态更新合并 (可以在 任何地方调用 setSate 方法，改变其中的某一个 key，最后都会跟原始对比，只是改变对应的值，其他不会变)
+
+            使用类就允许我们使用其它特性，例如局部状态、生命周期钩子
+            有两个属性 是 react 里 实现的 props（对外组件进行通信的），state（状态改变，会重新渲染）
+        2、对于事件处理中的this的理解；
+            我认为，在 react 中 用 类 来 初始化组价，最后并没有 实例化 class，而是 把 class 当做一个 “方法” 来运行，所以导致 class 里的方法 不存在  指向 class 的 this（这里回到值 输出 undefined）
+                而 在 angular2 中 用的 类，是通过 装饰器 把 类 实例化 了的(执行环境 就是当前 的 实例化对象)，所以 在 class 里 存在 class 方法的 this
+
+            在 react 中的 解决 办法 三种：
+            1、在 constructor 里 bind
+                如： constructor(){
+                    this.handleClick = this.handleClick.bind(this);
+                }
+            2、属性初始化器语法 （这个名字提出 https://doc.react-china.org/docs/handling-events.html）
+                如：class Ex{
+                   handleClick = ()=>{}  //这种写法
+                }
+            3、在回调函数 中 使用 箭头函数
+                (缺点: 使用这个语法有个问题就是每次 组件 渲染的时候都会创建一个不同的回调函数。在大多数情况下，这没有问题。然而如果这个回调函数作为一个属
+                性值传入低阶组件，这些组件可能会进行额外的重新渲染。我们通常建议在构造函数中绑定或使用属性初始化器语法来避免这类性能问题)
+                如：
+                    class LoggingButton extends React.Component {
+                      handleClick() {
+                        console.log('this is:', this);
+                      }
+
+                      render() {
+                        // This syntax ensures `this` is bound within handleClick
+                        return (
+                          <button onClick={(e) => this.handleClick(e)}> /*********这里***********/
+                            Click me
+                          </button>
+                        );
+                      }
+                    }
+        3、对于 事件中 传参 的写法 注意：
+            一定要不要 这样写 <div onClick={this.handleClick(参数)}></div> //这种方式，会直接 当 表达式 直接执行
+            两种写法：
+                <button onClick={(e) => this.deleteRow(id, e)}>Delete Row</button> （这种写法的 e 是 显示 传入,传入位置的顺序自己定） （e 是 事件对象）
+                <button onClick={this.deleteRow.bind(this, id)}>Delete Row</button> （这个是 隐式的 传入，就是不用写出来, 传入方式是最后传入）
+
     */
 
 
--- first_react_project/src/index.js
+++ first_react_project/src/index.js
@@ -11,10 +11,10 @@ import './index.css';
         )
     }
 }*/
-/*上面的简单写法*/
-/*function Square(props){  //这里的参数是 props
+// 上面的简单写法
+function Square(props){  //这里的参数是 props
     return (
-        <button className="square" onClick={props.Click}> {/!*!//这种写法就传不了值进来*!/}
+        <button className="square" onClick={props.Click}> {/*!//这种写法就传不了值进来*/}
             {props.value}
         </button>
     )
@@ -30,12 +30,12 @@ class Board extends React.Component {
     }
 
     render() {
-        /!*const status = `Next player: ${this.state.xIsNext?"X":"O"}`;*!/
+        /*const status = `Next player: ${this.state.xIsNext?"X":"O"}`;*/
         // const status = "Next player: "+(this.state.xIsNext?"X":"O");
 
         return (
             <div>
-                {/!*<div className="status">{2}</div>*!/}
+                {/*<div className="status">{2}</div>*/}
                 <div className="board-row">
                     {this.renderSquare(0)}
                     {this.renderSquare(1)}
@@ -56,19 +56,64 @@ class Board extends React.Component {
     }
 }
 
+function NumList(props){
+    let tempList = props.list;
+    let tempJsxList = tempList.map((n)=>{
+            return (
+                <li key={n.toString()}>
+                    {n}
+                </li>
+            )
+    });
+    return (
+        <ul>
+            {tempJsxList}
+        </ul>
+    )
+}
+
 class Game extends React.Component {
     constructor(){
         super();
         this.state = {
             squares:new Array(9).fill(null),
             xIsNext:true,
-        }
-
+        };
+        this.num = 1;
+        this.tryClick = this.tryClick.bind(this);
     }
+    /*生命周期*/
+    //当组件输出到 DOM 后
+    componentDidMount() {
+        let me = this;
+        (function ex(){
+            if(me.num==10)return;
+            (()=>{
+                console.log(me.num);
+                me.num++;
+            })();
+            me.timeStart = setTimeout(ex,1000);
+        })();
+        /*this.timeStart = setInterval(()=>{
+            console.log(this.num++);
+        },1000)*/
+    }
+    componentWillUnmount(){
+        // clearInterval(this.timeStart);
+        clearTimeout(this.timeStart);
+    }
+
     handleClick(i){
-        const squares = this.state.squares.slice(); /!*浅拷贝*!/
+        const squares = this.state.squares.slice(); /*浅拷贝*/
         squares[i] =this.state.xIsNext? 'X':'O';
         this.setState({squares: squares,xIsNext:!this.state.xIsNext});
+    }
+
+    tryClick(i,e,ev){
+        console.log(i);
+        console.log(e);
+        console.log(ev);
+        console.log("this: "+this);
     }
     render() {
         const status = "Next player: "+(this.state.xIsNext?"X":"O");
@@ -79,8 +124,12 @@ class Game extends React.Component {
                 </div>
                 <div className="game-info">
                     <div>{status}</div>
-                    <ol>{/!* TODO *!/}</ol>
+                    <ol>{/* TODO */}</ol>
                 </div>
+                {/*<div onClick={this.tryClick.bind(this,1,2)}>try event happening</div>*/}
+                <div onClick={(e)=>this.tryClick(e,1,2)}>try event happening</div>
+
+                <NumList list={[1,2,3,4]}/>
             </div>
         );
     }
@@ -91,9 +140,9 @@ class Game extends React.Component {
 ReactDOM.render(
     <Game />,
     document.getElementById('root')
-);*/
+);
 
-function tick() {
+/*function tick() {
     const element = (
         <div>
             <h1>Hello, world!</h1>
@@ -106,4 +155,4 @@ function tick() {
     );
 }
 
-setInterval(tick, 1000);
+setInterval(tick, 1000);*/
